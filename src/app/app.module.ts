c0496496b309b05ba73be7e50a804234a47afd0a
noelmaurice <noelmaurice4@gmail.com>
2019-11-16 18:04:03 +0100

    Commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,43 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
+import { HttpClientModule } from "@angular/common/http";
+import { Routes, RouterModule } from "@angular/router";
 
 import { AppComponent } from './app.component';
+import { MenuComponent } from './component/menu/menu.component';
+import { BannerComponent } from './component/banner/banner.component';
+import { PortfolioComponent } from './component/portfolio/portfolio.component';
+import { AboutComponent } from './component/about/about.component';
+import { ContactComponent } from './component/contact/contact.component';
+import { CopyrightComponent } from './component/copyright/copyright.component';
+import { FooterComponent } from './component/footer/footer.component';
+import { HomeComponent } from './component/home/home.component';
+
+const appRoutes: Routes = [
+  { path: '', component: HomeComponent, pathMatch: 'full' },
+  { path: '**', redirectTo: 'home' }
+];
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent,
+    BannerComponent,
+    PortfolioComponent,
+    AboutComponent,
+    ContactComponent,
+    CopyrightComponent,
+    FooterComponent,
+    HomeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    ReactiveFormsModule,
+    HttpClientModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
