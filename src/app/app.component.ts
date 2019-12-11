67f2a6b0c500b78d7260db8e25f5c64ff6056e22
noelmaurice <noelmaurice4@gmail.com>
2019-12-11 15:23:31 +0100

    Save

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import * as firebase from 'firebase';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,20 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'bootstrap';
+
+  constructor() {
+    const firebaseConfig = {
+      apiKey: "AIzaSyAq9sV-NIE3s-bQoo_8yJeW11RFzeQsDlw",
+      authDomain: "angular-nature.firebaseapp.com",
+      databaseURL: "https://angular-nature.firebaseio.com",
+      projectId: "angular-nature",
+      storageBucket: "angular-nature.appspot.com",
+      messagingSenderId: "950246924083",
+      appId: "1:950246924083:web:ce0c5eb8807df27396a846",
+      measurementId: "G-TYR0ZLB6CN"
+    };
+    // Initialize Firebase
+    firebase.initializeApp(firebaseConfig);
+    firebase.analytics();
+  }
 }
